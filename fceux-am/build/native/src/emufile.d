37e41e70486b90ded2db4ef4bd0e18598f241108
JOJO Young <211240045@smail.nju.edu.cn>
2022-09-17 17:36:18 +0800

    add files

---
+++ fceux-am/build/native/src/emufile.d
@@ -0,0 +1,9 @@
+/home/jojo/Documents/Project/ICS/fceux-am/build/native/src/emufile.o: \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/emufile.cpp \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/emufile.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/types.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/klib/include/klib.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/am.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/arch/native.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/amdev.h \
+ nes/gen/roms.h
---
+++ fceux-am/build/native/src/sound.d
@@ -0,0 +1,18 @@
+/home/jojo/Documents/Project/ICS/fceux-am/build/native/src/sound.o: \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/sound.cpp \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/types.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/klib/include/klib.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/am.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/arch/native.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/amdev.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/x6502.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/x6502struct.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/fceu.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/git.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/md5.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/../types.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/valuearray.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/sound.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/filter.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/state.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/config.h
