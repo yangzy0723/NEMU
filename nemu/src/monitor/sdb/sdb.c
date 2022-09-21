1f0cdb980f409095368e05062a082b0a95ad80ab
tracer-ics2022 <tracer@njuics.org>
2022-09-21 08:22:01 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:22:01 up 11:44,  1 user,  load average: 0.12, 0.12, 0.09

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -99,10 +99,10 @@ void sdb_set_batch_mode() {
 
 void sdb_mainloop() {
   if (is_batch_mode) {
+		printf("1");
     cmd_c(NULL);
     return;
   }
-	printf("1");
   for (char *str; (str = rl_gets()) != NULL; ) {
     char *str_end = str + strlen(str);
 
