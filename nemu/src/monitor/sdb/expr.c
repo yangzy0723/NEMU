f0b8d4a525b36daf901a865f4dd76f316daca7d8
tracer-ics2022 <tracer@njuics.org>
2022-09-26 22:25:59 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:25:59 up  1:17,  1 user,  load average: 0.15, 0.23, 0.29

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -21,7 +21,7 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
+  TK_NOTYPE = 256, TK_EQ, TK_NUM,
 
   /* TODO: Add more token types */
 
@@ -39,6 +39,10 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
+	{"-", '-'},						// subtraction
+	{"\\*", '*'},					// mul
+	{"/",'/'},						// divide
+	{"[0,9]*",TK_NUM},		// num
 };
 
 #define NR_REGEX ARRLEN(rules)
