11a33460e640384ae0ab232ba379f176acc083a3
tracer-ics2022 <tracer@njuics.org>
2022-09-27 19:57:37 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:57:37 up 22:49,  1 user,  load average: 0.16, 0.13, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -126,9 +126,10 @@ static bool make_token(char *e)//e是待解析的目标字符串。
 									}; break;		
           default: TODO();
         }
+        break;
       }
     }
-		break;
+
     if (i == NR_REGEX) 
 		{
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
