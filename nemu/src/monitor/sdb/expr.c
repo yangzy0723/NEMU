989264f3e9e689a239ce4b77adcaf588bc65fec2
tracer-ics2022 <tracer@njuics.org>
2022-09-27 19:49:16 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:49:16 up 22:40,  1 user,  load average: 0.41, 0.23, 0.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -120,9 +120,10 @@ static bool make_token(char *e)//e是待解析的目标字符串。
 					case TK_NUM:
 									{
 										tokens[nr_token].type = TK_NUM;
-										printf("%d\n",substr_len-1);
 										for(int j = substr_len-1; j >= 0; j--)
+										{
 												tokens[nr_token].str[substr_len - 1 - j] = substr_start[j]; //将123存入str数组的模式为3 2 1。
+										}
 										nr_token++;
 									}; break;		
           default: TODO();
