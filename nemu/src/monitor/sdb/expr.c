4bd03d1aacb45c63a90fee3bd4015c833c01c924
tracer-ics2022 <tracer@njuics.org>
2022-09-27 20:00:55 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:00:55 up 22:52,  1 user,  load average: 0.23, 0.26, 0.17

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -148,10 +148,10 @@ word_t expr(char *e, bool *success)
     *success = false;
     return 0;
   }
-	printf("123\n");
   /* TODO: Insert codes to evaluate the expression. */
 	/*for(int i = 0; i <= tokens_size-1; i++)
 		printf("%d ",tokens[i].type);*/
+	printf("%d\n",tokens_size);
 	return eval(success, 0, tokens_size - 1);//tokens_size记录tokens数组哪些位数有效，从而确定p，q。
 }
 
