acf48463c82263e58b9a61ad8c863e99b0ecf8e0
tracer-ics2022 <tracer@njuics.org>
2022-10-29 23:25:58 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:25:58 up  1:46,  1 user,  load average: 0.30, 0.26, 0.23

--- am-kernels/kernels/hello/hello.c
+++ am-kernels/kernels/hello/hello.c
@@ -1,13 +1,20 @@
 #include <am.h>
 #include <klib-macros.h>
-
+#include <klib.h>
 int main(const char *args) {
   const char *fmt =
     "Hello, AbstractMachine!\n"
     "mainargs = '%'.\n";
-
+	int d = 99999;
+	char s[100] = "$$$$$$$$$";
+	char buffer[100] = {0};
   for (const char *p = fmt; *p; p++) {
     (*p == '%') ? putstr(args) : putch(*p);
   }
+  printf("%d",printf("%s%d\n",s,d));
+  printf("%d",sprintf(buffer, "%s%d%s\n", s, d, s));
+  printf("%s\n",buffer);
+  printf("%d",snprintf(buffer, 5, "%s%d%s\n", s, d, s));
+  printf("%s",buffer);
   return 0;
 }
