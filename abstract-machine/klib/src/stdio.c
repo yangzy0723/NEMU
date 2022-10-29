e869e272a52f20a87170b9c0056d95ee9d829a5b
tracer-ics2022 <tracer@njuics.org>
2022-10-29 17:29:12 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:29:12 up 1 day, 22:14,  1 user,  load average: 0.20, 0.34, 0.56

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,24 +5,46 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-void write_int(int x, int *num)
+int write_int(int x)
 {
+	int num = 0;
 	if(x < 0)
 	{
 		putch('-');
-		(*num)++;
+		num++;
 		x = -x;
 	}
-	if(x>=10)
-		write_int(x/10, num);
-	putch(x%10+'0');
-	(*num)++;
-	return;
+	if(x >= 10)
+		num += write_int(x / 10);
+	putch(x % 10 + '0');
+	num++;
+	return num;
+}
+
+int write_int_buffer(int x, char **buffer)
+{
+	int num = 0;
+	if(x < 0)
+	{
+		*(*buffer) = '-';
+		(*buffer)++;
+		num++;
+		x = -x;
+	}
+	if(x >= 10)
+		num += write_int_buffer(x/10, buffer);
+	*(*buffer) = x % 10 + '0';
+	(*buffer)++;
+	num++;
+	return num;
 }
 
 int printf(const char *fmt, ...) {
 	va_list ap;
 	int char_num = 0;
+	int int_record;
+	char char_record;
+	char *string_record;
 	va_start(ap, fmt);
 	while(*fmt)
 	{
@@ -41,13 +63,13 @@ int printf(const char *fmt, ...) {
 					char_num++;
 					break;
 				case 'c':
-					char char_record = (char)va_arg(ap, int);
+					char_record = (char)va_arg(ap, int);
 					//这里va_arg参数为int的原因是，C中调用一个不带原型声明的函数，例如可变长参数函数，传入参数执行“默认实际参数提升”。char将提升为int。
 					putch(char_record);
 					char_num++;
 					break;
 				case 's':	
-					char* string_record = va_arg(ap, char*);
+					string_record = va_arg(ap, char*);
 					while(*string_record != 0)
 					{
 						putch(*string_record);
@@ -56,8 +78,8 @@ int printf(const char *fmt, ...) {
 					}
 					break; 
 				case 'd':	
-					int int_record = va_arg(ap, int);
-					write_int(int_record, &char_num);
+					int_record = va_arg(ap, int);
+					char_num += write_int(int_record);
 					break;
 				default:
 					return -1;
@@ -74,7 +96,60 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list ap;
+	int char_num = 0;
+	int int_record;
+	char char_record;
+	char *string_record;
+	va_start(ap, fmt);
+	while(*fmt)
+	{
+		if(*fmt != '%')
+		{
+			*out = *fmt;
+			out++;
+			char_num++;
+		}
+		else
+		{
+			fmt++;
+			switch(*fmt)
+			{
+				case '%':	
+					*out = '%';
+					out++;
+					char_num++;
+					break;
+				case 'c':
+					char_record = (char)va_arg(ap, int);
+					//这里va_arg参数为int的原因是，C中调用一个不带原型声明的函数，例如可变长参数函数，传入参数执行“默认实际参数提升”。char将提升为int。
+					*out = char_record;
+					out++;
+					char_num++;
+					break;
+				case 's':	
+					string_record = va_arg(ap, char*);
+					while(*string_record != 0)
+					{
+						*out = *string_record;
+						out++;
+						char_num++;
+						string_record++;
+					}
+					break; 
+				case 'd':	
+					int_record = va_arg(ap, int);
+					char_num += write_int_buffer(int_record, &out);
+					break;
+				default:
+					return -1;
+					break;
+			}
+		}
+		fmt++;
+	}
+	*out = 0;
+	return char_num;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
