8d21ce1aee5e399d610ff943ba7df29b9c862413
tracer-ics2022 <tracer@njuics.org>
2022-10-29 21:32:07 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:32:07 up 2 days,  2:17,  1 user,  load average: 0.40, 0.25, 0.22

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -39,6 +39,29 @@ int write_int_buffer(int x, char **buffer)
 	return num;
 }
 
+void write_int_buffer_limit(int x, int size, int *num, char **buffer)
+{
+	if(*num == size)
+			return;
+	if(x < 0)
+	{
+		*(*buffer) = '-';
+		(*buffer)++;
+		num++;
+		x = -x;
+	}
+	if(*num == size)
+		return;
+	if(x >= 10)
+		write_int_buffer_limit(x/10, size, num, buffer);
+	if(*num == size)
+		return;
+	*(*buffer) = x % 10 + '0';
+	(*buffer)++;
+	(*num)++;
+	return;
+}
+
 int printf(const char *fmt, ...) {
 	va_list ap;
 	int char_num = 0;
@@ -88,20 +111,15 @@ int printf(const char *fmt, ...) {
 		}
 		fmt++;
 	}
+	va_end(ap);
 	return char_num;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-	va_list ap;
 	int char_num = 0;
 	int int_record;
 	char char_record;
 	char *string_record;
-	va_start(ap, fmt);
 	while(*fmt)
 	{
 		if(*fmt != '%')
@@ -152,12 +170,86 @@ int sprintf(char *out, const char *fmt, ...) {
 	return char_num;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+int sprintf(char *out, const char *fmt, ...) {
+	va_list ap;
+	int char_num;
+	va_start(ap, fmt);
+	char_num = vsprintf(out, fmt, ap);
+	va_end(ap);
+	return char_num;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int char_num = 0;
+	int int_record;
+	char char_record;
+	char *string_record;
+	while(*fmt)
+	{
+		if(*fmt != '%')
+		{
+			*out = *fmt;
+			out++;
+			char_num++;
+			if(char_num == n)
+				return char_num;
+		}
+		else
+		{
+			fmt++;
+			switch(*fmt)
+			{
+				case '%':	
+					*out = '%';
+					out++;
+					char_num++;
+					if(char_num == n)
+							return char_num;
+					break;
+				case 'c':
+					char_record = (char)va_arg(ap, int);
+					//这里va_arg参数为int的原因是，C中调用一个不带原型声明的函数，例如可变长参数函数，传入参数执行“默认实际参数提升”。char将提升为int。
+					*out = char_record;
+					out++;
+					char_num++;
+					if(char_num == n)
+						return char_num;
+					break;
+				case 's':	
+					string_record = va_arg(ap, char*);
+					while(*string_record != 0)
+					{
+						*out = *string_record;
+						out++;
+						char_num++;
+						if(char_num == n)
+							return char_num;
+						string_record++;
+					}
+					break; 
+				case 'd':	
+					int_record = va_arg(ap, int);
+					write_int_buffer_limit(int_record, n, &char_num, &out);
+					if(char_num == n)
+						return char_num;
+					break;
+				default:
+					return -1;
+					break;
+			}
+		}
+		fmt++;
+	}
+	*out = 0;
+	return char_num;
 }
 
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  va_list ap;
+	int char_num;
+	va_start(ap, fmt);
+	char_num = vsnprintf(out, n, fmt, ap);
+	va_end(ap);
+	return char_num;
+}
 #endif
