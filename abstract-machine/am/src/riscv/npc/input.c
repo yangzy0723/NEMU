28f38117f7612541f4f257f4cdb5e8876215bb12
tracer-ics2022 <tracer@njuics.org>
2022-11-08 20:01:16 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:01:16 up  1:21,  1 user,  load average: 0.45, 0.19, 0.13

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -1,6 +1,7 @@
 #include <am.h>
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = inl(KBD_ADDR)>>15;
-  kbd->keycode = inl(KBD_ADDR)&0x000000ff;
+  uint32_t keyboard_value = inl(KBD_ADDR);
+  kbd->keydown = keyboard_value>>15;
+  kbd->keycode = keyboard_value&0x000000ff;
 }
