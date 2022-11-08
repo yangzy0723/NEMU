9a0e2c806256f92a9c62c46ea7d5f017ac2e1d9d
tracer-ics2022 <tracer@njuics.org>
2022-11-08 20:38:48 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:38:48 up  1:59,  1 user,  load average: 0.09, 0.12, 0.21

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,6 +5,6 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 	uint32_t am_scan_code = inl(KBD_ADDR);
-  kbd->keydown = am_scan_code >> 15;
-  kbd->keycode = am_scan_code & 0x000000ff;
+  kbd->keydown = am_scan_code >> 15;//第十六位是特征码，确定是按下按键还是抬起按键。
+  kbd->keycode = am_scan_code & 0x000000ff;//0-7位是按键的am对应码
 }
