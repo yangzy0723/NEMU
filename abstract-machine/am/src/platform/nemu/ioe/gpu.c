a7728f06ca158242fb6c1ea05a5cc568b8c3ef29
tracer-ics2022 <tracer@njuics.org>
2022-11-09 10:26:13 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:26:13 up  1:44,  1 user,  load average: 0.46, 0.43, 0.51

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -14,21 +14,19 @@ void __am_gpu_init() {
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 	uint32_t data_code = inl(VGACTL_ADDR);
-	uint16_t height = (uint16_t)(data_code & 0xffff);
-	uint16_t width = (uint16_t)(data_code >> 16);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = width, .height = height,
+    .width = (data_code >> 16) & 0x0000ffff, .height = data_code & 0x0000ffff,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) 
 {
-	uint32_t get_information = inl(VGACTL_ADDR);//获取设备长宽信息
-	uint32_t Width = (get_information >> 16) & 0x0000ffff;
+	/*uint32_t get_information = inl(VGACTL_ADDR);//获取设备长宽信息
+	uint32_t Width = (get_information >> 16) & 0x0000ffff;*/
 	/*uint32_t Height = get_information & 0x0000ffff; 用不到*/
-	uint32_t dy_x = ctl -> x;
+	/*uint32_t dy_x = ctl -> x;
 	uint32_t dy_y = ctl -> y;
 	int count = 0;
 	uint32_t *pixels_live = (uint32_t *)ctl -> pixels;
@@ -42,7 +40,18 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
 			count++;
 		}
 		dy_y++;
-	}
+	}*/
+int wth = io_read(AM_GPU_CONFIG).width;
+  uint32_t *fb_addr = (uint32_t *)FB_ADDR;
+  uint32_t *p_addr = (uint32_t *)ctl->pixels;
+  for (int i = 0; i < ctl->h; ++i) {
+    for (int j = 0; j < ctl->w; ++j) {
+      fb_addr[(ctl->y) * wth + i * wth + ctl->x + j] = p_addr[i * (ctl->w) + j];
+    }
+  }
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
