51ee48633c83113d478026e01c087c4b3dfa30ab
tracer-ics2022 <tracer@njuics.org>
2022-11-09 08:55:35 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:55:35 up 13 min,  1 user,  load average: 0.25, 0.30, 0.27

--- abstract-machine/am/src/platform/nemu/ioe/ioe.c
+++ abstract-machine/am/src/platform/nemu/ioe/ioe.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <klib-macros.h>
-
+#include <klib.h>
 void __am_timer_init();
 void __am_gpu_init();
 void __am_audio_init();
@@ -55,5 +55,5 @@ bool ioe_init() {
   return true;
 }
 
-void ioe_read (int reg, void *buf) { ((handler_t)lut[reg])(buf); }
+void ioe_read (int reg, void *buf) { printf("%d", reg); ((handler_t)lut[reg])(buf); }
 void ioe_write(int reg, void *buf) { ((handler_t)lut[reg])(buf); }
