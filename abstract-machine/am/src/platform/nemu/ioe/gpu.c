d285928dc99776a193a169a3dc041e4e0eb8b065
tracer-ics2022 <tracer@njuics.org>
2022-11-09 10:20:30 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:20:30 up  1:38,  1 user,  load average: 0.33, 0.23, 0.48

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,46 +1,41 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-	int w = 400;
-	int h = 300;
-	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-	for(int i = 0; i < w*h; i++)
-		fb[i] = i;
-	outl(SYNC_ADDR, 1);
+  // int i;
+  // int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
+  // int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-	uint32_t data_code = inl(VGACTL_ADDR);
+  uint32_t info = inl(VGACTL_ADDR);
+  uint16_t height = (uint16_t)(info & 0xFFFF);
+  uint16_t width = (uint16_t)(info >> 16);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = (data_code >> 16) & 0x0000ffff, .height = data_code & 0x0000ffff,
+    .width = width, .height = height,
     .vmemsz = 0
   };
 }
 
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) 
-{
-	uint32_t get_information = inl(VGACTL_ADDR);//获取设备长宽信息
-	uint32_t Width = (get_information >> 16) & 0x0000ffff;
-	/*uint32_t Height = get_information & 0x0000ffff; 用不到*/
-	uint32_t dy_x = ctl -> x;
-	uint32_t dy_y = ctl -> y;
-	int count = 0;
-	uint32_t *pixels_live = (uint32_t *)ctl -> pixels;
-  if (ctl->sync) 
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int wth = io_read(AM_GPU_CONFIG).width;
+  uint32_t *fb_addr = (uint32_t *)FB_ADDR;
+  uint32_t *p_addr = (uint32_t *)ctl->pixels;
+  for (int i = 0; i < ctl->h; ++i) {
+    for (int j = 0; j < ctl->w; ++j) {
+      fb_addr[(ctl->y) * wth + i * wth + ctl->x + j] = p_addr[i * (ctl->w) + j];
+    }
+  }
+  if (ctl->sync) {
     outl(SYNC_ADDR, 1);
-	for(int i = 0; i < ctl -> h; i++)
-	{
-		for(int j = 0; j < ctl -> w; j++)
-		{	
-			outl(FB_ADDR + Width * dy_y + dy_x + j, pixels_live[count]);
-			count++;
-		}
-		dy_y++;
-	}
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
