b5fe0c92e1c612cc97791b8c883c009788c919cf
tracer-ics2022 <tracer@njuics.org>
2022-11-09 09:27:45 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:27:45 up 45 min,  1 user,  load average: 1.40, 0.88, 0.53

--- abstract-machine/am/src/platform/nemu/ioe/ioe.c
+++ abstract-machine/am/src/platform/nemu/ioe/ioe.c
@@ -55,5 +55,5 @@ bool ioe_init() {
   return true;
 }
 
-void ioe_read (int reg, void *buf) { printf("yzy %d   ", reg); ((handler_t)lut[reg])(buf); }
-void ioe_write(int reg, void *buf) { printf("zqy %d   ", reg); ((handler_t)lut[reg])(buf); }
+void ioe_read (int reg, void *buf) {((handler_t)lut[reg])(buf); }
+void ioe_write(int reg, void *buf) {((handler_t)lut[reg])(buf); }
