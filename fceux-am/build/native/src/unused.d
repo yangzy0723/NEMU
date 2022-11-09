94f1960fd9d56c31c15f322c78a4a408a122ce50
tracer-ics2022 <tracer@njuics.org>
2022-11-09 10:52:12 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:52:12 up  2:10,  1 user,  load average: 0.84, 0.50, 0.43

--- fceux-am/build/native/src/unused.d
+++
@@ -1,8 +0,0 @@
-/home/jojo/Documents/Project/ICS/fceux-am/build/native/src/unused.o: \
- /home/jojo/Documents/Project/ICS/fceux-am/src/unused.cpp \
- /home/jojo/Documents/Project/ICS/fceux-am/src/types.h \
- /home/jojo/Documents/Project/ICS/abstract-machine/klib/include/klib.h \
- /home/jojo/Documents/Project/ICS/abstract-machine/am/include/am.h \
- /home/jojo/Documents/Project/ICS/abstract-machine/am/include/arch/native.h \
- /home/jojo/Documents/Project/ICS/abstract-machine/am/include/amdev.h \
- /home/jojo/Documents/Project/ICS/fceux-am/src/sound.h
--- fceux-am/build/native/src/utils/general.d
+++
@@ -1,7 +0,0 @@
-/home/jojo/Documents/Project/ICS/fceux-am/build/native/src/utils/general.o: \
- /home/jojo/Documents/Project/ICS/fceux-am/src/utils/general.cpp \
- /home/jojo/Documents/Project/ICS/fceux-am/src/utils/../types.h \
- /home/jojo/Documents/Project/ICS/abstract-machine/klib/include/klib.h \
- /home/jojo/Documents/Project/ICS/abstract-machine/am/include/am.h \
- /home/jojo/Documents/Project/ICS/abstract-machine/am/include/arch/native.h \
- /home/jojo/Documents/Project/ICS/abstract-machine/am/include/amdev.h
