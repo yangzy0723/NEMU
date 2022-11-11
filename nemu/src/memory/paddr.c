a14ffeeca4e96d11b1ee7e569a866901a70789e8
tracer-ics2022 <tracer@njuics.org>
2022-11-11 23:02:54 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:02:54 up 45 min,  1 user,  load average: 0.35, 0.37, 0.34

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -57,16 +57,22 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+#ifdef CONFIG_MTRACE
 	log_write("\tread data from ADDRESS " FMT_WORD ", the length of data is %d\n", addr, len);
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+#endif
+  
+	if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
 	log_write("\twrite data to ADDRESS " FMT_WORD " the length of data is %d, the data is " FMT_WORD "\n",addr, len, data);
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+#endif  
+
+	if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
