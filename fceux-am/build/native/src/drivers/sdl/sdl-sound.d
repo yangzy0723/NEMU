1113c328549fa45ef4e934b89682b327b846a9f7
tracer-ics2022 <tracer@njuics.org>
2022-11-11 23:51:19 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:51:19 up  1:34,  1 user,  load average: 0.50, 0.44, 0.33

---
+++ fceux-am/build/native/src/drivers/sdl/sdl-sound.d
@@ -0,0 +1,20 @@
+/home/jojo/Documents/Project/ICS/fceux-am/build/native/src/drivers/sdl/sdl-sound.o: \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/sdl-sound.cpp \
+ /home/jojo/Documents/Project/ICS/abstract-machine/klib/include/klib-macros.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/sdl.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/main.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/../../driver.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/../../types.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/klib/include/klib.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/am.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/arch/native.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/amdev.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/../../git.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/../../utils/md5.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/../../utils/../types.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/../../utils/valuearray.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/../../file.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/../../emufile.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/dface.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/input.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/sdl/../../config.h
---
+++ fceux-am/build/riscv32-nemu/src/cart.d
@@ -0,0 +1,20 @@
+/home/jojo/Documents/Project/ICS/fceux-am/build/riscv32-nemu/src/cart.o: \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/cart.cpp \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/types.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/klib/include/klib.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/am.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/arch/riscv32-nemu.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/amdev.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/fceu.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/git.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/md5.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/../types.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/valuearray.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/ppu.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/driver.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/file.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/emufile.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/cart.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/x6502.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/x6502struct.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/memory.h
