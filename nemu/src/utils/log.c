d162878623f32de6620b574fa6c550d1025bf35c
tracer-ics2022 <tracer@njuics.org>
2022-11-11 21:42:02 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:42:02 up 2 days, 13:00,  1 user,  load average: 0.17, 0.35, 0.35

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -19,9 +19,9 @@ extern uint64_t g_nr_guest_inst;
 FILE *log_fp = NULL;
 FILE *memlog_file;
 void init_log(const char *log_file) {
-  
+	printf("123\n"); 
 	memlog_file= fopen("../build/mem-log.txt", "w");
-  
+  printf("456\n");
 	log_fp = stdout;
   if (log_file != NULL) {
     FILE *fp = fopen(log_file, "w");
