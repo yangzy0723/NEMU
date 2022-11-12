fe7ef6760ea62e0db81bcae5e72096363e5fba13
tracer-ics2022 <tracer@njuics.org>
2022-11-12 10:33:35 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:33:35 up 12 min,  1 user,  load average: 0.36, 0.74, 0.61

---
+++ fceux-am/build/riscv32-nemu/nes/gen/mario.d
@@ -0,0 +1,2 @@
+/home/jojo/Documents/Project/ICS/fceux-am/build/riscv32-nemu/./nes/gen/mario.o: \
+ /home/jojo/Documents/Project/ICS/fceux-am/nes/gen/mario.c
---
+++ fceux-am/build/riscv32-nemu/src/video.d
@@ -0,0 +1,22 @@
+/home/jojo/Documents/Project/ICS/fceux-am/build/riscv32-nemu/src/video.o: \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/video.cpp \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/types.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/klib/include/klib.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/am.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/arch/riscv32-nemu.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/amdev.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/video.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/fceu.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/git.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/md5.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/../types.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/valuearray.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/file.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/emufile.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/memory.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/state.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/palette.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/input.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drawing.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/driver.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/drivers/common/vidblit.h
