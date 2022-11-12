f656e77a192fbf2bcda6e1fc7dba02c14654d2fa
tracer-ics2022 <tracer@njuics.org>
2022-11-12 10:38:10 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:38:10 up 17 min,  1 user,  load average: 0.04, 0.42, 0.52

--- fceux-am/build/riscv32-nemu/src/emufile.d
+++
@@ -1,9 +0,0 @@
-/home/jojo/Documents/Project/ICS/fceux-am/build/riscv32-nemu/src/emufile.o: \
- /home/jojo/Documents/Project/ICS/fceux-am/src/emufile.cpp \
- /home/jojo/Documents/Project/ICS/fceux-am/src/emufile.h \
- /home/jojo/Documents/Project/ICS/fceux-am/src/types.h \
- /home/jojo/Documents/Project/ICS/abstract-machine/klib/include/klib.h \
- /home/jojo/Documents/Project/ICS/abstract-machine/am/include/am.h \
- /home/jojo/Documents/Project/ICS/abstract-machine/am/include/arch/riscv32-nemu.h \
- /home/jojo/Documents/Project/ICS/abstract-machine/am/include/amdev.h \
- nes/gen/roms.h
