e1a73ee75fc068589bbe7ca28325c2507a690ed2
tracer-ics2022 <tracer@njuics.org>
2022-11-12 17:59:04 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:59:04 up  7:37,  1 user,  load average: 0.15, 0.25, 0.23

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -21,8 +21,9 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i = 0; i < 32; i++)
 		if(gpr(i) != ref_r->gpr[i])
 			return false;
-	/*if(ref_r->pc != pc)
-		return false;*/
+	printf(FMT_WORD""FMT_WORD, ref_r->pc, pc);
+	if(ref_r->pc != pc)
+		return false;
 	return true;
 }
 
