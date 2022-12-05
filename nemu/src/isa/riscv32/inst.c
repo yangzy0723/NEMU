459e04a778bde1e68660391e4c8c24fe9a7fa39c
tracer-ics2022 <tracer@njuics.org>
2022-12-05 18:18:16 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:18:16 up 17:28,  1 user,  load average: 0.14, 0.10, 0.09

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -114,6 +114,7 @@ static int decode_exec(Decode *s) {
   word_t src1 = 0, src2 = 0, imm = 0;
   s->dnpc = s->snpc;
 	int64_t tmp;
+	bool success;
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
   decode_operand(s, &dest, &src1, &src2, &imm, concat(TYPE_, type)); \
@@ -178,6 +179,7 @@ static int decode_exec(Decode *s) {
 	/*privileged instruction*/
 	INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, csrrs_function(imm, src1, dest));
 	INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, csrrw_function(imm, src1, dest));	
+	INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(isa_reg_str2val("a7", &success), s->snpc));
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
 
