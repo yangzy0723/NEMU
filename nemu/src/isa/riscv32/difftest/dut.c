fb7319e93ec76a5b656165ce518ac5dc71bd4ca5
tracer-ics2022 <tracer@njuics.org>
2022-12-05 23:39:59 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:39:59 up 22:49,  1 user,  load average: 0.11, 0.15, 0.10

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,12 +17,20 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+#include <stdio.h>
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  /*for(int i = 0; i < 32; i++)
+  for(int i = 0; i < 32; i++)
 		if(gpr(i) != ref_r->gpr[i])
+		{
+			printf("register %d is different\n",i);
 			return false;
+		}
 	if(ref_r->pc != cpu.pc)
-		return false;*/
+	{
+		printf("pc is different\n");
+		return false;
+	}
 	return true;
 }
 
