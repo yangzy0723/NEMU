e8d0f0b7976b58ea776e7d4e742862cb25f30127
tracer-ics2022 <tracer@njuics.org>
2022-12-11 22:57:40 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:57:40 up 54 min,  1 user,  load average: 0.36, 0.27, 0.19

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,13 @@
 #include <common.h>
 #include "syscall.h"
+
+#define STRACE 1
 void do_syscall(Context *c) {
+
+#ifdef STRACE
+	printf("System call trace is triggered:\nmcause\tGPR1\tGPR2\tGPR3\tGPR4\n%p\t%p\t%p\t%p\t%p", c->mcause, c->GPR1, c->GPR2, c->GPR3, c->GPR4);
+#endif
+	
   uintptr_t a[4];
   a[0] = c->GPR1;
   switch (a[0]) {
