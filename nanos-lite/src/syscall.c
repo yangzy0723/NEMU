ef7debd756572ac1ce8c2b684d87a0db855b18b6
tracer-ics2022 <tracer@njuics.org>
2022-12-13 11:34:35 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:34:35 up 17:55,  1 user,  load average: 0.34, 0.32, 0.25

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,7 +6,7 @@ size_t fs_lseek(int fd, size_t offset, int whence);
 size_t fs_read(int fd, void *buf, size_t len);
 int fs_close(int fd);
 
-#define STRACE 1
+//#define STRACE 1
 void do_syscall(Context *c) {
 
 #ifdef STRACE
