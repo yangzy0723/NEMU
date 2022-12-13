d2d4c8a8cdb71d08f1ceff838430377477c83d10
tracer-ics2022 <tracer@njuics.org>
2022-12-13 19:33:34 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:33:34 up 1 day,  1:54,  1 user,  load average: 0.85, 0.64, 0.49

--- navy-apps/tests/time-test/time-test.c
+++ navy-apps/tests/time-test/time-test.c
@@ -1,11 +1,11 @@
 #include <sys/time.h>
 #include <stdio.h>
+		struct timeval now_time;
 int main()
 {
 	while(1)
 	{
 		int i = 1;
-		struct timeval now_time;
 		gettimeofday(&now_time, NULL);
 		printf("sec:%d, usec:%d\n",(int)now_time.tv_sec, (int)now_time.tv_usec);
 		if(now_time.tv_usec % 100000 == 0)
