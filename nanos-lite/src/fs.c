473b786c429f1207fd5f0b8a23446e7197e620d9
tracer-ics2022 <tracer@njuics.org>
2022-12-14 03:51:55 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:51:55 up  6:39,  1 user,  load average: 0.37, 0.36, 0.27

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -56,6 +56,7 @@ int fs_open(const char *pathname, int flags, int mode)
 
 size_t fs_read(int fd, void *buf, size_t len)
 {
+	printf("123\n");
 	if(file_table[fd].read != NULL)
 		return file_table[fd].read(buf, 0, len);//从键盘读入不考虑偏移量啦
 	else if(fd == FD_STDIN || fd == FD_STDOUT || fd == FD_STDERR)
