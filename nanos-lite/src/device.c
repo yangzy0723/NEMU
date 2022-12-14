3b411a9663b756883a76f5188e18152821ac24ca
tracer-ics2022 <tracer@njuics.org>
2022-12-14 11:30:49 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:30:49 up 14:18,  1 user,  load average: 0.50, 0.98, 1.24

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,10 +22,11 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
 	AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+	printf("keydown: %d; keycode: %d\n", ev.keydown, ev.keycode);
 	if(ev.keycode == AM_KEY_NONE)
 		return 0;
 	char* keyboard_code;
-	if(!ev.keydown)
+	if(ev.keydown)
 		keyboard_code = "kd ";
 	else
 		keyboard_code = "ku ";
