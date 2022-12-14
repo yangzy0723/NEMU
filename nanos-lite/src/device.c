b85387406ab5e6ea1c32a3b26babab97f5c541a4
tracer-ics2022 <tracer@njuics.org>
2022-12-14 11:36:26 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:36:26 up 14:23,  1 user,  load average: 1.39, 1.25, 1.28

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -35,7 +35,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 
 	//assert(len >= strlen(keyboard_code) + 1);
 	memcpy(buf, (const void *)keyboard_code, strlen(keyboard_code));	
-	*(char *)(buf + strlen(keyboard_code) + 1) = '\n';
+	//*(char *)(buf + strlen(keyboard_code) + 1) = '\n';
 	return strlen(keyboard_code) + 1;
 }
 
