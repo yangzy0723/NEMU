d1be5d7c8c7c0318ad5391d80bb3ef4f2744fd4f
tracer-ics2022 <tracer@njuics.org>
2022-12-14 11:42:45 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:42:45 up 14:30,  1 user,  load average: 0.89, 0.84, 1.05

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -24,8 +24,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 	AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
 	if(ev.keycode == AM_KEY_NONE)
 		return 0;
-	printf("keydown: %d; keycode: %d\n", ev.keydown, ev.keycode);
-	char* keyboard_code = 0;
+	char* keyboard_code = (char *)buf;
 	memset(keyboard_code, 0, strlen(keyboard_code));
 	if(ev.keydown)
 		keyboard_code = "kd ";
