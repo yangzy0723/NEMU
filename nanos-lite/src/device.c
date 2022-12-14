1811905f5fcc4334971a4102642e27c3c49c1c69
tracer-ics2022 <tracer@njuics.org>
2022-12-14 11:26:40 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:26:40 up 14:14,  1 user,  load average: 1.81, 1.60, 1.47

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,7 +25,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 	if(ev.keycode == AM_KEY_NONE)
 		return 0;
 	char* keyboard_code;
-	if(ev.keydown)
+	if(!ev.keydown)
 		keyboard_code = "kd ";
 	else
 		keyboard_code = "ku ";
