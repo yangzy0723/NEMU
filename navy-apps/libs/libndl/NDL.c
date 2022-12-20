31070263a7c45b5b5dea88aa693bc08d5b5832b8
tracer-ics2022 <tracer@njuics.org>
2022-12-20 22:32:27 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:32:27 up  2:39,  1 user,  load average: 0.14, 0.15, 0.11

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -76,20 +76,15 @@ int NDL_Init(uint32_t flags) {
 	close(fd);
 	int my_weight = 0;
 	int weight_constant = 8;
-	printf("%c\n", buf[8]);
 	int count = 0;
-	while('0' <= buf[weight_constant] <= '9')
+	while('0' <= buf[weight_constant] && buf[weight_constant] <= '9')
 	{
-		printf("%c\n", buf[weight_constant]);
-		count++;
-		if(count >= 100)
-				break;
 		my_weight = my_weight * 10 + *(buf + weight_constant) - '0';
 		weight_constant++;
 	}
 	int my_height = 0;
 	int height_constant = 19;
-	while('0' <= buf[height_constant] <= '9')
+	while('0' <= buf[height_constant] && buf[height_constant] <= '9')
 	{
 		my_height = my_height * 10 + *(buf + height_constant) - '0';
 		height_constant++;
