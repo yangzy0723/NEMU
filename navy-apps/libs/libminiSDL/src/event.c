e6d6c96799c7fc8638b70b71b8a5984f7105d58d
tracer-ics2022 <tracer@njuics.org>
2022-12-22 11:20:18 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:20:18 up 13:19,  1 user,  load average: 0.42, 0.36, 0.30

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -14,7 +14,31 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+	char buf[64];
+	int if_event;
+	if_event = NDL_PollEvent(buf, sizeof(buf));
+	if(buf[1] == 'd')
+		ev->type = SDL_KEYDOWN;
+	else if(buf[1] == 'u')
+		ev->type = SDL_KEYUP;
+	else
+		printf("Not implemented yet\n");
+	for(int i = 0; i < sizeof(keyname)/sizeof(keyname[0]); i++)
+	{
+		char revise_char[64];
+		memset(revise_char, 0, sizeof(revise_char));
+		strcat(revise_char, keyname[i]);
+		strcat(revise_char, "\n");
+		if(strcmp(&buf[3], revise_char) == 0)
+		{
+			ev->key.keysym.sym = i;
+			break;
+		}
+	}
+	if(if_event)
+		return 1;
+	else
+		return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
