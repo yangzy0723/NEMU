824c8eb7a85055f83066313c651354aa7571c9e5
tracer-ics2022 <tracer@njuics.org>
2022-12-22 11:49:26 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:49:26 up 13:49,  1 user,  load average: 0.10, 0.30, 0.34

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -50,7 +50,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
 	int fd = open("/dev/fb", O_RDWR);
-	for(int i = 0; i < h; i++)
+	for(int i = 0; i < h; i++)//一次写一行的像素颜色信息进去
 	{
 		lseek(fd, ((y + i) * screen_w + x) * 4, SEEK_SET);
 		write(fd, pixels + w * i, w * 4);
