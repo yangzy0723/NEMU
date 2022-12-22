131731c546f74f8f489c49e1e6912b6544a34370
tracer-ics2022 <tracer@njuics.org>
2022-12-22 15:56:09 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:56:09 up 52 min,  1 user,  load average: 0.06, 0.21, 0.32

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -63,7 +63,7 @@ word_t paddr_read(paddr_t addr, int len) {
   
 	if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  out_of_bound(addr);
+  //out_of_bound(addr);
   return 0;
 }
 
