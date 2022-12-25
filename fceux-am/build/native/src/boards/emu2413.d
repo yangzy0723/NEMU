1a015e9a10f5492803cc3b57bcb8f70bd8eaae7c
tracer-ics2022 <tracer@njuics.org>
2022-12-25 14:12:10 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:12:10 up 7 min,  1 user,  load average: 1.45, 1.32, 0.68

---
+++ fceux-am/build/native/src/boards/emu2413.d
@@ -0,0 +1,8 @@
+/home/jojo/Documents/Project/ICS/fceux-am/build/native/src/boards/emu2413.o: \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/boards/emu2413.c \
+ /home/jojo/Documents/Project/ICS/abstract-machine/klib/include/klib.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/am.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/arch/native.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/amdev.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/boards/emu2413.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/boards/../types.h
---
+++ fceux-am/build/native/src/ppu.d
@@ -0,0 +1,26 @@
+/home/jojo/Documents/Project/ICS/fceux-am/build/native/src/ppu.o: \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/ppu.cpp \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/types.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/klib/include/klib.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/am.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/arch/native.h \
+ /home/jojo/Documents/Project/ICS/abstract-machine/am/include/amdev.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/x6502.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/x6502struct.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/fceu.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/git.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/md5.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/../types.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/valuearray.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/ppu.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/sound.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/file.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/emufile.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/utils/memory.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/cart.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/palette.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/state.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/video.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/input.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/driver.h \
+ /home/jojo/Documents/Project/ICS/fceux-am/src/pputile.inc
