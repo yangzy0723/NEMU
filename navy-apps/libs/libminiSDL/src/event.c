d2c5e2d3ba658c0bce9de5591c67a313f1ab395f
tracer-ics2022 <tracer@njuics.org>
2022-12-30 15:47:21 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:47:21 up 57 min,  1 user,  load average: 0.40, 0.24, 0.23

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -10,6 +10,8 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
+static uint8_t key_state[sizeof(keyname)/sizeof(keyname[0])] = {0};
+
 int SDL_PushEvent(SDL_Event *ev) {
 	assert(0);
   return 0;
@@ -39,6 +41,10 @@ int SDL_PollEvent(SDL_Event *ev) {
 		if(strcmp(&buf[3], revise_char) == 0)
 		{
 			ev->key.keysym.sym = i;
+			if(ev->type == SDL_KEYDOWN)
+				key_state[i] = 1;
+			else
+				key_state[i] = 0;
 			break;
 		}
 	}
@@ -67,6 +73,10 @@ int SDL_WaitEvent(SDL_Event *event) {
 		strcat(revise_char, "\n");
 		if(strcmp(&buf[3], revise_char) == 0)
 		{
+			if(event->type == SDL_KEYDOWN)
+				key_state[i] = 1;
+			else
+				key_state[i] = 0;
 			event->key.keysym.sym = i;
 			break;
 		}
@@ -80,6 +90,7 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-	assert(0);
-  return NULL;
+  assert(numkeys != NULL);
+	*numkeys = sizeof(keyname)/sizeof(keyname[0]);
+	return key_state;
 }
