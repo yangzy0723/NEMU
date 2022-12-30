d6c4246a414e54461b33405a6e34a0c6de500c7f
tracer-ics2022 <tracer@njuics.org>
2022-12-30 16:20:48 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:20:48 up  1:30,  1 user,  load average: 0.72, 0.65, 0.48

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -94,9 +94,6 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 			for(int j = 0; j < dstrect->w; j++)
 				((uint32_t *)(dst->pixels))[(dstrect->y + i) * dst->w + dstrect->x + j] = color;
 }
-static inline uint32_t translate_color(SDL_Color *color){
-  return (color->a << 24) | (color->r << 16) | (color->g << 8) | color->b;
-}
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 	if(s->format->BitsPerPixel == 32)
 	{
@@ -112,39 +109,23 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 		}
 	}
 	else if(s->format->BitsPerPixel == 8)
-	{    if (w == 0 && h == 0 && x ==0 && y == 0){
-      w = s->w; h = s->h;
-      x = 0;    y = 0;
-    }
-
-    uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
-    assert(pixels);
-    uint8_t *src = (uint8_t *)s->pixels;
-
-    for (int i = 0; i < h; ++i){
-      for (int j = 0; j < w; ++j){
-        pixels[i * w + j] = translate_color(&s->format->palette->colors[src[(y + i) * s->w + x + j]]);
-        //pixels[i * w + j] = s->format->palette->colors[src[(y + i) * s->w + x + j]].val;
-      }
-    }
-    NDL_DrawRect(pixels, x, y, w, h);
-
-    free(pixels);
-		//int draw_w = w;
-		//int draw_h = h;
-		//if(w == 0 && h == 0 && x == 0 && y == 0)
-		//{
-			//draw_w = s->w;
-			//draw_h = s->h;
-		//}
-		//uint32_t color[draw_w * draw_h + 5];
-		//for(int i = 0; i < draw_h; i++)
-			//for(int j = 0; j < draw_w; j++)
-			//{
-				//SDL_Color the_color = s->format->palette->colors[((uint8_t*)(s->pixels))[(y + i) * s->w + x + j]]; 
-				//color[i * draw_w + j] = (the_color.a << 24) | (the_color.r << 16) | (the_color.g << 8) | the_color.b;
-			//}
-		//NDL_DrawRect(color, x, y, draw_w, draw_h);
+	{
+		int draw_w = w;
+		int draw_h = h;
+		if(w == 0 && h == 0 && x == 0 && y == 0)
+		{
+			draw_w = s->w;
+			draw_h = s->h;
+		}
+		uint32_t *color = malloc(draw_w * draw_h * 4);
+		for(int i = 0; i < draw_h; i++)
+			for(int j = 0; j < draw_w; j++)
+			{
+				SDL_Color the_color = s->format->palette->colors[((uint8_t*)(s->pixels))[(y + i) * s->w + x + j]]; 
+				color[i * draw_w + j] = (the_color.a << 24) | (the_color.r << 16) | (the_color.g << 8) | the_color.b;
+			}
+		NDL_DrawRect(color, x, y, draw_w, draw_h);
+		free(color);
 	}
 }
 
