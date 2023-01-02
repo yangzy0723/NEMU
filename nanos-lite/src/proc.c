cfd5273d0a8b490913b353601412a9244dccdf4d
tracer-ics2022 <tracer@njuics.org>
2023-01-02 17:07:19 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:07:19 up 17 min,  1 user,  load average: 0.47, 0.42, 0.36

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -16,15 +16,15 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
+    Log("Hello World from Nanos-lite with arg '%s' for the %dth time!", (char *)arg, j);
     j ++;
     yield();
   }
 }
 
 void init_proc() {
-	context_kload(&pcb[0], hello_fun, NULL);
-	context_kload(&pcb[1], hello_fun, NULL);
+	context_kload(&pcb[0], hello_fun, "zqy");
+	context_kload(&pcb[1], hello_fun, "yzy");
   switch_boot_pcb();
 
   //Log("Initializing processes...");
