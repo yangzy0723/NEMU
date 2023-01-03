ea6512242bc19eb34fd838097804692fd7c94bf3
tracer-ics2022 <tracer@njuics.org>
2023-01-03 22:11:52 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:11:52 up  3:34,  1 user,  load average: 1.02, 0.58, 0.33

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -10,7 +10,7 @@ void call_main(uintptr_t *args) {
 	char **argv = (char **)(args + 1);
 	char **envp = (char **)(args + 1 + argc + 1);
   environ = envp;
-	printf("%s\n", *(argv + 2));
+	printf("%s", (char *)(args + 3));
   exit(main(argc, argv, envp));
   assert(0);
 }
