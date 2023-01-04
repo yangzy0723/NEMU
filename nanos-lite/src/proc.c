0955a1c3c40dce604152f7677c1f005fdfe13911
tracer-ics2022 <tracer@njuics.org>
2023-01-04 13:29:20 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:29:20 up 18:51,  1 user,  load average: 1.01, 0.55, 0.35

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -42,8 +42,8 @@ Context* schedule(Context *prev) {
 
 int execve(char *filename, char *const argv[], char *const envp[])
 {
-	context_uload((current == &pcb[0] ? &pcb[1] : &pcb[0]), filename, argv, envp);
-	//switch_boot_pcb();
+	context_uload(&pcb[1], filename, argv, envp);
+	switch_boot_pcb();
 	yield();
 	return 0;
 }
