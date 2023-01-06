a68824dd49a7d496c5e9c4ed7636dfb51b5f34b4
tracer-ics2022 <tracer@njuics.org>
2023-01-06 19:38:51 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:38:51 up 1 day,  3:11,  1 user,  load average: 0.28, 0.24, 0.12

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -65,6 +65,10 @@ void context_kload(PCB *pcb, void(*entry)(void *), void *arg)
 
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[])
 {
+	protect(&(pcb->as));//可以理解为创建了一个进程的页目录，同时腾出了(0x40000000, 0x80000000)的用户进程空间
+	void *alloc_p_end = new_page(8);//32KB
+	for(int i = 1; i <= 8; i++)
+		map(&(pcb->as), (pcb->as).area.end - i * PGSIZE, alloc_p_end - i * PGSIZE, 1);//都是以页为单位	
 	//此处需要先进行栈的维护，进行argv和envp的处理，否则envp信息会丢失
 	//count
 	int num_argv = 0;
@@ -77,10 +81,11 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 	{
 		num_envp++;
 	}
+
 	//deal with string-area
 	char *record_position_argv[num_argv];
 	char *record_position_envp[num_envp];
-	char *p = (char *)new_page(8);//32KB
+	char *p = (pcb->as).area.end;
 	for(int i = 0; i < num_argv; i++)
 	{
 		p = p - strlen(argv[i]) - 1;
