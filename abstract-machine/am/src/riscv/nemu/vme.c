5e38344e922e91539150a8cc7ad1fe312abae365
tracer-ics2022 <tracer@njuics.org>
2023-01-06 13:35:19 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:35:19 up 21:08,  1 user,  load average: 0.60, 0.29, 0.20

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -98,10 +98,12 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 	
 	if(((*page_directory_item_entry) & PTE_V) == 0)//研究其有效位是否为0,若为0说明二级表未分配
 	{
+		printf("%p\n", *page_directory_item_entry);
 		(*page_directory_item_entry) = ((*page_directory_item_entry) & 0x000003ff) + (PTE)pgalloc_usr(PGSIZE);
 		//高22位，存页表的地址，低2位一定为0
 		(*page_directory_item_entry) = ((*page_directory_item_entry) | PTE_V);
 		//有效位为1,说明这个页目录项对应的页表有在工作了
+		printf("%p\n", *page_directory_item_entry);
 	}
 
 	uintptr_t page_table_entry = GET_BASE_ADDR(*page_directory_item_entry);//得到页表基地址
