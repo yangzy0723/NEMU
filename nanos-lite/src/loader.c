a4d979704cec7a1fe5fe3b1fa62fb8365f85f434
tracer-ics2022 <tracer@njuics.org>
2023-01-07 18:58:12 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:58:12 up 2 days,  2:31,  1 user,  load average: 0.19, 0.33, 0.25

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -36,8 +36,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 		{
 			int num_page = segment.p_memsz / PGSIZE + 1;
 			void *start = new_page(num_page) - num_page * PGSIZE;
-			//printf("%s申请了%d页内存，起始地址为%p\n", filename, num_page, (uintptr_t)start);
 			void *vaddr = (void *)segment.p_vaddr;
+			printf("%s申请了%d页内存，虚地址为%p，起始地址为%p\n", filename, num_page, (uintptr_t)vaddr, (uintptr_t)start);
 			for(int i = 0; i < num_page; i++)
 				map(&(pcb->as), (void *)(((uint32_t)vaddr & 0xfffff000) + i * PGSIZE), (void *)(start + i * PGSIZE), 0);
 			
