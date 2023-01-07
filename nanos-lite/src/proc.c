d9558116064ee5c24783e175e76d50e32ae45721
tracer-ics2022 <tracer@njuics.org>
2023-01-07 19:40:26 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:40:26 up 2 days,  3:13,  1 user,  load average: 0.32, 0.13, 0.13

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,7 +25,7 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   switch_boot_pcb();
-	//context_kload(&pcb[0], hello_fun, "yzy && zqy");
+	context_kload(&pcb[0], hello_fun, "yzy && zqy");
 	context_uload(&pcb[1], "/bin/pal", NULL, NULL);
 
 	Log("Initializing processes...");
