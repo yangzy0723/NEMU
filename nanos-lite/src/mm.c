ddb2b69b2b1bf6f936169450c0dd8af1106221b8
tracer-ics2022 <tracer@njuics.org>
2023-01-07 20:14:59 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:14:59 up 2 days,  3:47,  1 user,  load average: 0.49, 0.37, 0.29

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,19 +1,21 @@
 #include <memory.h>
+#include <stdint.h>
 #include <proc.h>
 
 static void *pf = NULL;
 
 void* new_page(size_t nr_page) {
-  pf = pf + nr_page * PGSIZE;
-	return pf;
-}//每一页分配nr_page*4KB的连续内存区域，得到的是末尾地址，都是对齐了的
+  //nr_page * 4KB
+  pf += nr_page * 4 * 1024;
+  return pf - nr_page * 4 * 1024;
+}
 
 #ifdef HAS_VME
 static void* pg_alloc(int n) {
-	int nr_page = n / PGSIZE;
-	void *ret_point = new_page(nr_page);
-	memset(ret_point - nr_page * PGSIZE, 0, nr_page * PGSIZE);
-	return ret_point - nr_page * PGSIZE;
+  assert(n % (4096) == 0);
+  void *ret = new_page(n / (4096));
+  memset(ret, 0, n);
+  return ret;
 }
 #endif
 
@@ -21,32 +23,21 @@ void free_page(void *p) {
   panic("not implement yet");
 }
 
-/* The brk() system call handler. */
-//我的理解，传入的参数直接是增量
+//#define align4k_page_number(x) (x % 4096 == 0 ? x / 4096 : (x / 4096 + 1))
+
 extern PCB *current;
-/*int mm_brk(uintptr_t brk) {
-	if((int32_t)brk < 0)
-		return 0;
-	else
-	{
-		printf("current -> max_brk :%p\n", current->max_brk);
-		int pre_page = (current->max_brk)/PGSIZE;
-		int now_page = (current->max_brk + brk)/PGSIZE;
-		int num_new_page = now_page - pre_page;
-		void *alloc_p_start = new_page(num_new_page) - PGSIZE * num_new_page;
-		for(int i = 0; i < num_new_page; i++)
-			map(&(current->as), (void *)((current->max_brk & 0xfffff000) + PGSIZE + i * PGSIZE), alloc_p_start + i * PGSIZE, 0);
-		current->max_brk = current->max_brk + brk;
-		return 0;
-	}
-}*/
+/* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
   uintptr_t max_page_end = current->max_brk; 
   uintptr_t max_page_pn = (max_page_end >> 12) - 1;
   uintptr_t brk_pn = brk >> 12;//12
+
+  //[page_start, page_end) 所以，应该是大于等于就重新分配
+  //printf("申请内存 max_page_end: %p\t to brk: %p\n", max_page_end, brk);//分配从[max_page_pn, brk_pn]的所有页
   if (brk >= max_page_end){
-    void *allocted_page =  new_page(brk_pn - max_page_pn + 1);
+    void *allocted_page =  new_page(brk_pn - max_page_pn + 1);//需要申请的新的页数
     for (int i = 0; i < brk_pn - max_page_pn + 1; ++i){
+      //TODO: prot 有问题
       map(&current->as, (void *)(max_page_end + i * 0xfff),
        (void *)(allocted_page + i * 0xfff), 1);
     }
@@ -54,12 +45,12 @@ int mm_brk(uintptr_t brk) {
     current->max_brk = (brk_pn + 1) << 12;
     assert(current->max_brk > brk);
   }
+
   return 0;
 }
 
-
 void init_mm() {
-  pf = (void *)ROUNDUP(heap.start, PGSIZE);//赋初值的时候就对齐了，低12位为0
+  pf = (void *)ROUNDUP(heap.start, PGSIZE);
   Log("free physical pages starting from %p", pf);
 
 #ifdef HAS_VME
---
+++ nanos-lite/src/mm1.c
@@ -0,0 +1,68 @@
+/*#include <memory.h>
+#include <proc.h>
+
+static void *pf = NULL;
+
+void* new_page(size_t nr_page) {
+  pf = pf + nr_page * PGSIZE;
+	return pf;
+}//每一页分配nr_page*4KB的连续内存区域，得到的是末尾地址，都是对齐了的
+
+#ifdef HAS_VME
+static void* pg_alloc(int n) {
+	int nr_page = n / PGSIZE;
+	void *ret_point = new_page(nr_page);
+	memset(ret_point - nr_page * PGSIZE, 0, nr_page * PGSIZE);
+	return ret_point - nr_page * PGSIZE;
+}
+#endif
+
+void free_page(void *p) {
+  panic("not implement yet");
+}
+*/
+/* The brk() system call handler. */
+//我的理解，传入的参数直接是增量
+/*extern PCB *current;*/
+/*int mm_brk(uintptr_t brk) {
+	if((int32_t)brk < 0)
+		return 0;
+	else
+	{
+		printf("current -> max_brk :%p\n", current->max_brk);
+		int pre_page = (current->max_brk)/PGSIZE;
+		int now_page = (current->max_brk + brk)/PGSIZE;
+		int num_new_page = now_page - pre_page;
+		void *alloc_p_start = new_page(num_new_page) - PGSIZE * num_new_page;
+		for(int i = 0; i < num_new_page; i++)
+			map(&(current->as), (void *)((current->max_brk & 0xfffff000) + PGSIZE + i * PGSIZE), alloc_p_start + i * PGSIZE, 0);
+		current->max_brk = current->max_brk + brk;
+		return 0;
+	}
+}*/
+/*int mm_brk(uintptr_t brk) {
+  uintptr_t max_page_end = current->max_brk; 
+  uintptr_t max_page_pn = (max_page_end >> 12) - 1;
+  uintptr_t brk_pn = brk >> 12;//12
+  if (brk >= max_page_end){
+    void *allocted_page =  new_page(brk_pn - max_page_pn + 1);
+    for (int i = 0; i < brk_pn - max_page_pn + 1; ++i){
+      map(&current->as, (void *)(max_page_end + i * 0xfff),
+       (void *)(allocted_page + i * 0xfff), 1);
+    }
+
+    current->max_brk = (brk_pn + 1) << 12;
+    assert(current->max_brk > brk);
+  }
+  return 0;
+}
+
+
+void init_mm() {
+  pf = (void *)ROUNDUP(heap.start, PGSIZE);//赋初值的时候就对齐了，低12位为0
+  Log("free physical pages starting from %p", pf);
+
+#ifdef HAS_VME
+  vme_init(pg_alloc, free_page);
+#endif
+}*/
