74abe72594bea126c90c827e015967490d198681
tracer-ics2022 <tracer@njuics.org>
2023-01-10 19:54:22 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:54:22 up  2:14,  1 user,  load average: 0.60, 0.48, 0.43

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -1,7 +1,6 @@
 #include <am.h>
 #include <nemu.h>
 #include <klib.h>
-#include <riscv/riscv.h>
 
 static AddrSpace kas = {};
 static void* (*pgalloc_usr)(int) = NULL;
@@ -30,8 +29,6 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
   pgfree_usr = pgfree_f;
 
   kas.ptr = pgalloc_f(PGSIZE);
-  printf("kas.ptr addr: %p\n", kas.ptr);
-
   int i;
   for (i = 0; i < LENGTH(segments); i ++) {
     void *va = segments[i].start;
@@ -39,10 +36,8 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
       map(&kas, va, va, 0);
     }
   }
-
   set_satp(kas.ptr);
   vme_enable = 1;
-
   return true;
 }
 
@@ -59,20 +54,34 @@ void unprotect(AddrSpace *as) {
 }
 
 void __am_get_cur_as(Context *c) {
-  if (c->pdir != NULL){ //自行添加
-    //printf("在__am_get_cur_as中设置为由%p，地址为%p，更改为，", c->pdir, &c->pdir);
-    c->pdir = (vme_enable ? (void *)get_satp() : NULL);
-    //printf("%p\n", c->pdir);
-  }
+  c->pdir = (vme_enable ? (void *)get_satp() : NULL);
 }
 
 void __am_switch(Context *c) {
   if (vme_enable && c->pdir != NULL) {
-    //printf("在__am_switch中设置satp为%p\n", c->pdir);
     set_satp(c->pdir);
   }
 }
 
+static inline uintptr_t GET_DIR(uintptr_t p)//页目录索引
+{
+	return (p & (uintptr_t)0xffc00000) >> 22;
+}
+
+static inline uintptr_t GET_PAGE(uintptr_t p)//页表索引
+{
+	return (p & (uintptr_t)0x003ff000) >> 12;
+}
+
+static inline uintptr_t GET_OFFSET(uintptr_t p)//页内偏移量
+{
+	return p & 0x00000fff;
+}
+
+static inline uintptr_t GET_BASE_ADDR(PTE p)//得到基地址
+{
+	return (uintptr_t)p & 0xfffffc00;
+}
 #define VA_VPN_0(x) (((uintptr_t)x & 0x003FF000u) >> 12)
 #define VA_VPN_1(x) (((uintptr_t)x & 0xFFC00000u) >> 22)
 #define VA_OFFSET(x) ((uintptr_t)x & 0x00000FFFu)
@@ -113,3 +122,40 @@ Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
 
   return context;
 }
+//先只考虑有效位
+/*void map(AddrSpace *as, void *va, void *pa, int prot) {
+	//参考ICS课本图6.45和jianshu.com/6780c4ac272e，但要注意这里是riscv32架构
+	
+	//printf("处理v:%p, p:%p\n", (uintptr_t)va, (uintptr_t)pa);	
+	uintptr_t page_directory_entry = (uintptr_t)as->ptr;//页目录的基地址
+	//printf("基地址%p\n", page_directory_entry);	
+	PTE *page_directory_item_entry = (PTE *)(page_directory_entry + GET_DIR((uintptr_t)va) * 4);
+	//10位*4,正好4096字节，一页
+	//此处需要*4得到对应的页目录项，因为每个页目录项都是4个字节的
+	
+	//printf("页目录值%p\n", (*page_directory_item_entry));	
+	//printf("页目录地址%p\n", page_directory_item_entry);
+	if(((*page_directory_item_entry) & PTE_V) == 0)//研究其有效位是否为0,若为0说明二级表未分配
+	{
+		(*page_directory_item_entry) = ((*page_directory_item_entry) & 0x000003ff) + (PTE)pgalloc_usr(PGSIZE);
+		//高22位，存页表的地址，低2位一定为0
+		(*page_directory_item_entry) = ((*page_directory_item_entry) | PTE_V);
+		//有效位为1,说明这个页目录项对应的页表有在工作了
+	}
+
+	uintptr_t page_table_entry = GET_BASE_ADDR(*page_directory_item_entry);//得到页表基地址
+	
+	PTE *page_table_item_entry = (PTE *)(page_table_entry + GET_PAGE((uintptr_t)va) * 4);//得到页表项的地址
+	(*page_table_item_entry) = ((*page_table_item_entry) & 0x000003ff) + (PTE)GET_BASE_ADDR((uintptr_t)pa >> 2);//取34位的高22位填充
+	(*page_table_item_entry) = (*page_table_item_entry) | PTE_V;
+
+	printf("page_table_item_entry: %p\n", page_table_item_entry);
+}
+
+Context *ucontext(AddrSpace *as, Area ustack, void *entry) {
+  Context *context = ustack.end - sizeof(Context);
+	context->mepc = (uintptr_t)entry;
+	context->mstatus = 0x1800;
+	context->pdir = as->ptr;
+	return context;
+}*/
