7493aaaab0958564090ca955f3dcec941473f57e
tracer-ics2022 <tracer@njuics.org>
2023-01-10 20:00:47 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:00:47 up  2:21,  1 user,  load average: 0.26, 0.40, 0.42

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -15,8 +15,8 @@ static Area segments[] = {      // Kernel memory mappings
 
 static inline void set_satp(void *pdir) {
   uintptr_t mode = 1ul << (__riscv_xlen - 1);
-	printf("pdir: %p\n", (uint32_t)pdir);
   asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
+	printf("123\n");
 }
 
 static inline uintptr_t get_satp() {
