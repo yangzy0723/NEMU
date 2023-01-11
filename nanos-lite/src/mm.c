d50ee020c30fed205e385c8bc93f1c0a5d3ece76
tracer-ics2022 <tracer@njuics.org>
2023-01-11 14:30:05 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:30:05 up  2:15,  1 user,  load average: 0.14, 0.27, 0.24

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -33,6 +33,7 @@ int mm_brk(uintptr_t brk) {
 		int num_page = now_page - pre_page + 1;
 		void *alloc_p_start = new_page(num_page) - PGSIZE * num_page;
 		//printf("max_brk:%p\n", current->max_brk);
+		printf("%d\n", num_page);
 		for(int i = 0; i < num_page; i++)
 			map(&(current->as), (void *)((current->max_brk & 0xfffff000) + i * PGSIZE), alloc_p_start + i * PGSIZE, 0);
 		current->max_brk = brk;
