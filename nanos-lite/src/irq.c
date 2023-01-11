eaf2c8717c78f3b8516b98d7b39ad17cddc6d3ed
tracer-ics2022 <tracer@njuics.org>
2023-01-11 18:54:52 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:54:52 up  6:40,  1 user,  load average: 0.75, 0.40, 0.21

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -7,7 +7,7 @@ static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 		case EVENT_YIELD: printf("yield!\n"); break; 
 		case EVENT_SYSCALL: do_syscall(c); break;
-		case EVENT_IRQ_TIMER:Log("12345"); c = schedule(c); break;
+		case EVENT_IRQ_TIMER:c = schedule(c); break;
     default: panic("Unhandled event ID = %d", e.event);break;
   }
 
