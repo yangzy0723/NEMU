38e52e8384abcff3a2355c0d71ff8fc0edbe73ec
tracer-ics2022 <tracer@njuics.org>
2023-01-11 19:10:39 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:10:39 up  6:56,  1 user,  load average: 0.19, 0.07, 0.10

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -87,7 +87,7 @@ static inline uintptr_t GET_BASE_ADDR(PTE p)//得到基地址
 void map(AddrSpace *as, void *va, void *pa, int prot) {
 	//参考ICS课本图6.45和jianshu.com/6780c4ac272e，但要注意这里是riscv32架构
 	//printf("va %p, pa %p\n", (uintptr_t)va, (uintptr_t)pa);	
-	printf("处理v:%p, p:%p\n", (uintptr_t)va, (uintptr_t)pa);	
+	//printf("处理v:%p, p:%p\n", (uintptr_t)va, (uintptr_t)pa);	
 	uintptr_t page_directory_entry = (uintptr_t)as->ptr;//页目录的基地址
 	//printf("基地址%p\n", page_directory_entry);	
 	PTE *page_directory_item_entry = (PTE *)(page_directory_entry + GET_DIR((uintptr_t)va) * 4);
