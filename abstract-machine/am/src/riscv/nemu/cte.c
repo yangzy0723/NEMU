97cfaa25e65145e230ea7b2edd17c9058cbded6e
tracer-ics2022 <tracer@njuics.org>
2023-01-14 14:29:06 +0800

    >  compile NEMU 211240045 杨镇源 Linux jojo-laptop 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:29:06 up 18:24,  1 user,  load average: 0.12, 0.04, 0.03

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -13,7 +13,7 @@ Context* __am_irq_handle(Context *c) {
     switch (c->mcause) {
 			case -1: ev.event = EVENT_YIELD; break;
       case 0x80000007: ev.event = EVENT_IRQ_TIMER; break;
-			case 0:case 1:case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10: 
+			case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10: 
 			case 11: case 12: case 13: case 14: case 15: case 16: case 17: case 18: case 19:
 							 ev.event = EVENT_SYSCALL; break;
 			default: ev.event = EVENT_ERROR; break;
@@ -41,7 +41,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 	Context *context = kstack.end - sizeof(Context);
 	context->mepc = (uintptr_t)entry;
-	context->mstatus = 0x1800 | 0x80;
+	context->mstatus = 0x1800;
 	context->GPR2 = (uintptr_t)arg;
 	context->pdir = NULL;
 	return context;
